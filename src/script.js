9406288999a355eb6c6746872872832b1c74b5cf
Bruno santos <bruno.csantos@salvador.ba.gov.br>
2025-02-20 15:09:31 -0300

    ajustando

--- src/script.js
+++ src/script.js
@@ -61,7 +61,7 @@ document.querySelector('button[type="submit"]').addEventListener('click', functi
 }, false);
 
 function showAndHideErrorMessage() {
-  const errMessage = document.querySelector('.error');
+  const errorMessage = document.querySelector('.error');
   errorMessage.style.display = 'block';
   scroll(0, 0);
   hideMessageAfterTimeout(errorMessage);
